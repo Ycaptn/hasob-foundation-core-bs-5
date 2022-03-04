c6338e172a39ed39d89a8012f86128784b3c58f2
maitiigor <oluwatosinadesoyesalami@gmail.com>
2022-03-04 12:52:17 +0100

    adding send button to discussion comment

--- resources/views/components/comment-entry.blade.php
+++ resources/views/components/comment-entry.blade.php
@@ -10,6 +10,7 @@
                     type="text" class="form-control input-sm" 
                     placeholder="Type in your comments and press enter to save comments"
             />
+            <span class='input-group-addon' class="btn-send-comment"><a href="#" id="btn-send-comment" class="btn-send-comment"><i class='img-circle img-sm fa fa-paper-plane' style='font-size:25px;padding-top:2px;'></i></a></span>
         </div>
 
     </div>
@@ -21,6 +22,68 @@
             $('#comment_primary_id').hide()
             $("#comment-text").on('keypress', function(e){
                 if (e.which==13 && $('#comment-text').val().length > 2){
+                    $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('input[name="_token"]').val()}});
+                    $('#comment_primary_id').show()
+                    e.preventDefault();
+                    let spinner = '<div class="loader2" id="loader-1"></div>';
+                    var formData = new FormData();
+                    if( $('#comment_primary_id').val() == "0"){
+                        options = JSON.stringify({
+                        'commentable_id': '{{ $commentable->id }}',
+                        'commentable_type':  String.raw`{{ get_class($commentable) }}`,
+                        'comments':$('#comment-text').val(),
+                        });
+                    }else{
+                        options = JSON.stringify({
+                        'commentable_id': '{{ $commentable->id }}',
+                        'commentable_type':  String.raw`{{ get_class($commentable) }}`,
+                        'comments':$('#comment-text').val(),
+                        'id': $('#comment_primary_id').val()
+                        });
+                    }
+                   
+                    formData.append('options', options);
+                    swal({
+                        html: true,
+                        title: 'Submitting Post Please Wait!',
+                        text:  spinner,
+                        showCancelButton: false, 
+                        showConfirmButton: false
+                    });
+
+                    $.ajax({
+                        url: "{{ route('fc.comment-add') }}",
+                        type: 'POST',
+                        processData: false,
+                        contentType: false,
+                        data: formData,
+                        success: function(data){
+                            swal.close();
+                            if (data!=null && data.status=='fail'){
+                                if (data.response!=null){     
+                                    alert("Error submitting comments "+data.response);
+                                }
+                            }else if (data!=null && data.status=='ok'){
+                                setTimeout(() => {
+                                    alert("Comments have been saved.");
+                                    location.reload();
+                                }, 1000);
+                                
+                            }else{
+                                alert("Error submitting comments");
+                            }
+                        },
+                        error: function(data){
+                            swal.close();
+                            console.log(data);
+                        }
+                    });
+                }
+            });
+
+            $("#btn-send-comment").on('click', function(e){
+                e.preventDefault();
+                if ( $('#comment-text').val().length > 2){
                     $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('input[name="_token"]').val()}});
                     $('#comment_primary_id').show()
                     e.preventDefault();
