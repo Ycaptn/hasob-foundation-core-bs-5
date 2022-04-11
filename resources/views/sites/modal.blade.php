0d7c167f37bb665c700d71a21ad3043dbf1182a2
samcesa45 <samuel@hasob.com.ng>
2022-04-11 12:30:35 +0100

    added site reload functionality

--- resources/views/sites/modal.blade.php
+++ resources/views/sites/modal.blade.php
@@ -265,11 +265,11 @@ $(document).ready(function() {
                     });
                 }else{
                     $('#div-site-modal-error').hide();
-                    window.setTimeout( function(){
+                    $('#div-site-modal-error').hide();
+                  
                         //window.alert("The Site saved successfully.");
                         //swal("Saved", "Site saved successfully.", "success");
 
-                        $('#div-site-modal-error').hide();
 
                         swal({
                                 title: "Saved",
@@ -280,11 +280,13 @@ $(document).ready(function() {
                                 confirmButtonClass: "btn-success",
                                 confirmButtonText: "OK",
                                 closeOnConfirm: false
-                            },function(){
-                                location.reload(true);
-                        });
+                            })
 
-                    },20);
+                            setTimeout(function(){
+                                location.reload(true);
+                        }, 1000);
+
+                   
                 }
 
                 $("#spinner-sites").hide();
