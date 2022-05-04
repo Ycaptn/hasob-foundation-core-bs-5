bac61bf3fc0b0c7f1634345fe99be1e65db59be8
samcesa45 <samuel@hasob.com.ng>
2022-05-05 00:23:28 +0530

    minor fixes

--- resources/views/users/partials/user-display.blade.php
+++ resources/views/users/partials/user-display.blade.php
@@ -8,65 +8,71 @@ $current_user = Auth::user();
 
     <input type='hidden' id="idUserDetails" name="idUserDetails" />
 
-    <div class="mb-3">
-        <label class="form-label">Title</label>
-        <div class="col-lg-2" style="padding-top:7px">
+    <div class="mb-3 d-flex " style="padding-top:7px">
+    <div class='me-3'>
+        <strong>Name</strong>
+
+    </div>
+       <div >
             {{ $current_user->title }}
-        </div>
-    </div>
-
-    <div class="mb-3">
-        <label class="form-label">Name</label>
-        <div class="col-lg-3" style="padding-top:7px">
             {{ $current_user->first_name }}
-        </div>
-        <div class="col-lg-3">
             {{ $current_user->middle_name }}
-        </div>
-    </div>
-
-    <div class="mb-3">
-        <label class="form-label">Last Name</label>
-        <div class="col-lg-6" style="padding-top:7px">
             {{ $current_user->last_name }}
         </div>
     </div>
 
-    <div class="mb-3">
-        <label class="form-label">Telephone</label>
-        <div class="col-lg-6" style="padding-top:7px">
+     <div class="mb-3 d-flex " style="padding-top:7px">
+    <div class='me-3'>
+            <strong>Telephone</strong>
+
+        </div>
+        <div>
             {{ $current_user->telephone }}
+
         </div>
+        
     </div>
 
-    <div class="mb-3">
-        <label class="form-label">Email Address</label>
-        <div class="col-lg-6" style="padding-top:7px">
-            {{ $current_user->email }}
-        </div>
-    </div>
+    <div class="mb-3 d-flex " style="padding-top:7px">
+    <div class='me-3'>
+            <strong class="form-label">Email Address</strong>
 
-    <div class="mb-3">
+        </div>
+       <div>
+
+           {{ $current_user->email }}
+       </div>
+       
+    </div>
+    {{-- <div class="mb-3">
         <label class="form-label">Password</label>
         <div class="col-lg-3" style="padding-top:7px">******</div>
-    </div>
+    </div> --}}
 
+<div class="mb-3 d-flex align-items-center " style="padding-top:7px">
+    <div class='me-3'>
 
-    <div class="mb-3">
-        <label class="form-label">Department</label>
-        <div class="col-lg-6" style="padding-top:7px">
+            <strong class="form-label">Department</strong>
+        </div>
+        <div>
             @if ($current_user->department != null)
             {{ strtoupper($current_user->department->long_name) }}
             @endif
+
         </div>
     </div>
 
-    <div class="mb-3">
-        <label class="form-label">Job Title</label>
-        <div class="col-lg-6" style="padding-top:7px">
+    <div class="mb-3 d-flex align-items-center " style="padding-top:7px">
+    <div class='me-3'>
+            <strong class="form-label">Job Title</strong>
+
+        </div>
+        <div>
             {{ $current_user->job_title }}
+
         </div>
     </div>
 
 </form>
 
+   
