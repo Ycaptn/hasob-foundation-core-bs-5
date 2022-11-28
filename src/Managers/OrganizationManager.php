71005cbc4df3c1df412dbb02fb48e1ee7042df13
Emjay <durowadeabdulmajeed@gmail.com>
2022-11-28 21:20:52 +0100

    fix

--- src/Managers/OrganizationManager.php
+++ src/Managers/OrganizationManager.php
@@ -22,10 +22,10 @@ class OrganizationManager {
     
     public function loadTenant($identifier) {
 
-        //if (Schema::hasTable('fc_organizations') == false){
-          //  Log::error("Unable to set Organization ID as organizations table doesn't exist on DB");
-            //return null;
-        //}
+        if (Schema::hasTable('fc_organizations') == false){
+           Log::error("Unable to set Organization ID as organizations table doesn't exist on DB");
+            return null;
+        }
 
         if ($identifier == "127.0.0.1" || $identifier == "localhost"){
 
