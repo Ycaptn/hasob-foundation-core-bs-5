4a475b1eb5f195560aaaf6a98c14d333bbb44e4b
bosahc <bosah@hasobnigeria.com>
2022-12-07 01:07:06 +0100

    attachment capture implemenation

--- resources/views/components/attachment-capture.blade.php
+++ resources/views/components/attachment-capture.blade.php
@@ -4,7 +4,7 @@
 </button>
 
 <div class="modal fade" id="{{$control_id}}_capture-modal" tabindex="-1" role="dialog" aria-labelledby="{{$control_id}}_capture-modal-label" aria-hidden="true">
-    <div class="modal-dialog modal-md" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 id="{{$control_id}}_capture-modal-title" class="modal-title">Capture</h4>
@@ -16,13 +16,24 @@
                     <span id="error_msg_{{$control_id}}_capture"></span>
                 </div>
 
-                <div class="capture-error hide-cont" role="alert">
-                    Unable to capture image. Please pick a brighter spot to continue.<br><br>
-                </div>
-                <div class="{{$control_id}}_capture-area">
-                    <div id="{{$control_id}}_camera"></div>
-                    <div id="{{$control_id}}_snapShotArea"></div>
-                    <button type="button" class="btn btn-xs btn-primary" id="{{$control_id}}_capture-btn" value="add">Capture</button>
+                <div class="row">
+                    <div class="d-flex align-items-center mb-2">
+                        <div>
+                            <div class="{{$control_id}}_capture-area">
+                                <div id="{{$control_id}}_camera"></div>
+                                <div id="{{$control_id}}_snapShotArea"></div>
+                                <center>
+                                    <button type="button" class="btn btn-sm btn-primary m-1 px-3" id="{{$control_id}}_capture-btn"><i class="fa fa-camera"></i> Capture</button>
+                                    <button type="button" class="btn btn-sm btn-primary m-1 px-3" id="{{$control_id}}_save-img-btn"><i class="fa fa-file-image-o"></i> Save Images</button>
+                                    <button type="button" class="btn btn-sm btn-primary m-1 px-3" id="{{$control_id}}_save-pdf-btn"><i class="fa fa-file-pdf-o"></i> Save PDF</button>
+                                </center>
+                            </div>
+                        </div>
+                        <div class="flex-grow-1 ms-3">
+                            <ul id="{{$control_id}}-ul-img_lister" class="list-unstyled">
+                            </ul>
+                        </div>
+                    </div>
                 </div>
 
             </div>
@@ -31,29 +42,66 @@
     </div>
 </div>
 
+<div class="modal fade" id="{{$control_id}}_editor-modal" tabindex="-1" role="dialog" aria-labelledby="{{$control_id}}_editor-modal-label" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 id="{{$control_id}}_editor-modal-title" class="modal-title">Editor</h4>
+                <button type="button" class="btn-close close_editor-modal" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div id="{{$control_id}}_editable_container">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="{{$control_id}}_saver-modal" tabindex="-1" role="dialog" aria-labelledby="{{$control_id}}_saver-modal-label" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 id="{{$control_id}}_saver-modal-title" class="modal-title">Save</h4>
+                <button type="button" class="btn-close close_saver-modal" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 @push('page_scripts')
+
+    <script type="text/javascript" src="https://scaleflex.cloudimg.io/v7/plugins/filerobot-image-editor/latest/filerobot-image-editor.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.js"></script>
     <script type="text/javascript" src="https://unpkg.com/face-api.js@0.22.2/dist/face-api.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script type="text/javascript">
 
         $(document).ready(function() {
+
+            window.jsPDF = window.jspdf.jsPDF;
+            let image_store = [];
 
             $('#{{$control_id}}_btnNewCapture').click(function() {
                 $('#error_div_{{$control_id}}_capture').hide();
                 
                 navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
                 navigator.getMedia({video: true}, function() {
-                        //Webcam is available
                         $('#{{$control_id}}_capture-modal').modal('show');
                     }, function() {
                         alert("No Camera");
                     }
                 );
 
-                // CAMERA SETTINGS.
                 Webcam.set({
-                    width: 420,
-                    height: 320,
+                    width: 528,
+                    height: 380,
+                    dest_width: 640,
+                    dest_height: 480,
                     image_format: 'jpeg',
                     jpeg_quality: 100
                 });
@@ -66,86 +114,136 @@
                 location.reload(true);
             });
 
-            let tries = 0;
-
-
             $(document).on('click', '#{{$control_id}}_capture-btn', function(e) {
                 e.preventDefault();
-                tries++
-                $(this).text('Capturing...');
-                $('.capture-error').fadeOut(300);
+                $('#error_div_{{$control_id}}_capture').fadeOut(300);
                 captureImage();
             })
 
             function captureImage() {
                 Webcam.snap(function (data_uri) {
-                    detectFace(data_uri);
+                    //$('#{{$control_id}}_capture-btn').text('Capture');
+                    image_store.push(data_uri);
+                    listCapturedImages();
                 });
             }
 
-            async function detectFace(data_uri){
-                
-                const image = new Image();
-                image.src= data_uri;
-                await faceapi.nets.tinyFaceDetector.loadFromUri('/face-models-service');
-                
-                //const landmarks = await faceapi.detectAllFaces(image, new faceapi.TinyFaceDetectorOptions()).withLandmarks();
-                const faces = await faceapi.detectAllFaces(image, new faceapi.TinyFaceDetectorOptions());
-                    
-                // console.log(faces);
-
-                if (faces.length === 0 && tries <= 2) {
-                console.log("no face detected");
-                $('#capture-btn').text('Capture');
-                $('.capture-error').html(' Unable to capture image. Please pick a brighter spot to continue.').fadeIn(300);
-                }else if(faces.length === 0 && tries > 2){
-                    sendPicture(data_uri);
-                }
-                if(faces.length > 1){
-                    console.log("multiple faces detected");
-                    $('#capture-btn').text('Capture');
-                    $('.capture-error').html('Unable to process. Multiple faces detected.').fadeIn(300);
-                }
-                if (faces.length === 1){
-                    if(faces[0]._score < 0.4 || tries > 2){
-                        //console.log("image quality is low");
-                        sendPicture(data_uri);
-                    }else if(faces[0]._score > 0.4){
-                        console.log("image quality is good ");
-
-                        sendPicture(data_uri);
-                    }
-                // console.log("face detected"); 
-                }
+            function listCapturedImages(){
+                $('#{{$control_id}}-ul-img_lister').empty();
+                $.each(image_store, function(key,item){
+                    $('#{{$control_id}}-ul-img_lister').append(
+                        `<li class="d-flex align-items-center border-bottom pb-2">
+                            <div class="product-box">
+                                <img src="`+item+`"/>
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <p class="fwt-bold mt-0 mb-0">Page `+(key+1)+`</p>
+                                <a href="#" class="{{$control_id}}_edit-btn" data-val="`+key+`" data-img="`+item+`"><i class="bx bx-edit"></i></a>
+                                <a href="#" class="{{$control_id}}_delete-btn" data-val="`+key+`"><i class="bx bx-trash"></i></a>
+                            </div>
+                        </li>`
+                    );
+                });
             }
 
-            function sendPicture(data_uri) {
-                // console.log(data_uri);
-                // $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('input[name="_token"]').val()}});
-                // let actionType = "POST";
-                // let endPointUrl = $("input[name='save_url']").val();;
-                // join_url = $("input[name='join_url']").val();
+            $(document).on('click', '.{{$control_id}}_delete-btn', function(e) {
+                image_store.splice($(this).attr('data-val'), 1);
+                listCapturedImages();
+            });
 
-                // let formData = new FormData();
-                // formData.append('student_img', data_uri);
-                // document.getElementById('snapShotArea').innerHTML = 
-                // '<br><img src="' + data_uri + '" width="70px" height="50px" />';
-                // $.ajax({
-                //     url:endPointUrl,
-                //     type: "POST",
-                //     data: formData, 
-                //     cache: false,
-                //     processData:false, 
-                //     contentType: false,
-                //     dataType: 'json',
-                //     success: function(result){
-                //         $('#capture-btn').text('Redirecting...');
-                //         window.open(join_url, "_blank");
-                //     },
-                // });
-            } 
+            $(document).on('click', '.{{$control_id}}_edit-btn', function(e) {
+                let editorConfig = {
+                    source: $(this).attr('data-img'),
+                    onSave: function(editedImageObject, designState) {
+                        image_store[$(this).attr('data-val')] = designState.imageBase64;
+                        listCapturedImages();
+                    }.bind(this, image_store),
+                    annotationsCommon: {fill: '#ff0000'},
+                    Text: { text: 'Filerobot...' },
+                    Rotate: { angle: 90, componentType: 'slider' },
+                    translations: {
+                        profile: 'Profile',
+                        coverPhoto: 'Cover photo',
+                        facebook: 'Facebook',
+                        socialMedia: 'Social Media',
+                        fbProfileSize: '180x180px',
+                        fbCoverPhotoSize: '820x312px',
+                    },
+                    Crop: {
+                        presetsItems: [
+                            {
+                                titleKey: 'classicTv',
+                                descriptionKey: '4:3',
+                                ratio: 4 / 3,
+                                // icon: CropClassicTv, // optional, CropClassicTv is a React Function component. Possible (React Function component, string or HTML Element)
+                            },
+                            {
+                                titleKey: 'cinemascope',
+                                descriptionKey: '21:9',
+                                ratio: 21 / 9,
+                                // icon: CropCinemaScope, // optional, CropCinemaScope is a React Function component.  Possible (React Function component, string or HTML Element)
+                            },
+                        ],
+                        presetsFolders: [{}],
+                    },
+                    //tabsIds: [window.FilerobotImageEditor.TABS.ADJUST, window.FilerobotImageEditor.TABS.ANNOTATE, window.FilerobotImageEditor.TABS.WATERMARK], // or ['Adjust', 'Annotate', 'Watermark']
+                    tabsIds: [window.FilerobotImageEditor.TABS.ADJUST],
+                    defaultTabId: window.FilerobotImageEditor.TABS.ADJUST, // or 'Annotate'
+                    defaultToolId: window.FilerobotImageEditor.TOOLS.TEXT, // or 'Text'
+                };
 
+                let filerobotImageEditor = new FilerobotImageEditor(
+                    document.querySelector('#{{$control_id}}_editable_container'), editorConfig
+                );
 
+                filerobotImageEditor.render({
+                    onClose: (closingReason) => {
+                        console.log('Closing reason', closingReason);
+                        filerobotImageEditor.terminate();
+                    }
+                });
+
+                $('#{{$control_id}}_editor-modal').modal('show');
+            });
+
+            function saveAttachable(){
+                $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('input[name="_token"]').val()}});
+
+                let actionType = "POST";
+                let endPointUrl = $("input[name='save_url']").val();;
+
+                let formData = new FormData();
+                formData.append('student_img', data_uri);
+                
+                $.ajax({
+                    url:endPointUrl,
+                    type: "POST",
+                    data: formData, 
+                    cache: false,
+                    processData:false, 
+                    contentType: false,
+                    dataType: 'json',
+                    success: function(result){
+                        $('#capture-btn').text('Redirecting...');
+                    },
+                });
+            }
+
+            function savePDF() {
+                let pdf = new jsPDF();
+                $.each(image_store, function(key,item){
+                    pdf.addImage(item, 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
+                    if(key<(image_store.length-1)){ pdf.addPage(); }
+                });
+                pdf.save("index.pdf");
+            }
+
+            $(document).on('click', '#{{$control_id}}_save-img-btn', function(e) {
+            });
+
+            $(document).on('click', '#{{$control_id}}_save-pdf-btn', function(e) {
+                savePDF();
+            });
 
         });
 
