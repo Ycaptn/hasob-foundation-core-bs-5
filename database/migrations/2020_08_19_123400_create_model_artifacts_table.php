cabd57ca67ac863e42ae1adb2505e903174082c7
bosahc <bosah@hasobnigeria.com>
2023-04-30 16:03:40 +0100

    better indexing added for performance improvements to model_artifacts table ... when dealing with large number of artifacts the database slows down.

--- database/migrations/2020_08_19_123400_create_model_artifacts_table.php
+++ database/migrations/2020_08_19_123400_create_model_artifacts_table.php
@@ -35,7 +35,9 @@ class CreateModelArtifactsTable extends Migration
             $table->timestamps();
         });
         Schema::enableForeignKeyConstraints();
-        //DB::update("ALTER TABLE fc_model_artifacts AUTO_INCREMENT = 5524;");
+
+        //AAny artifact stored in the table needs to be keyed on the model_name, id, and key values for speed and optimization.
+        DB::update("ALTER TABLE `fc_model_artifacts` ADD INDEX `fc_model_artifacts_model_name_id_key` (`model_name`, `model_primary_id`, `key`);");
     }
 
     /**
