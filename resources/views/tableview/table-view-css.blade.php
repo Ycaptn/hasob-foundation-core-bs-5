4ebaf705faf52d6a177ee200ba5ec295539a632c
ajagabos007 <ajagabos007@gmail.com>
2023-08-21 09:06:21 +0100

    add table data view

---
+++ resources/views/tableview/table-view-css.blade.php
@@ -0,0 +1,23 @@
+<style type="text/css">
+
+    .tbv-txt-light-blue {
+        color: #86b7f3;
+    }
+    .tbv-txt-light-dark-blue {
+        color: #4e44e7;
+    }
+    .tbv-current-page{
+        background-color: #008cff !important;
+        color: #fff !important;
+    }
+    .disable{
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+    .disable-link{
+        pointer-events: none;
+    }       
+            
+  
+    
+</style>
