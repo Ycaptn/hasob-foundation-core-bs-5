32141020a4cbe5a0eca1a80e146a553da304114a
apollosgeofrey <apollos@hasob.ng>
2023-08-27 22:16:09 +0100

    improvement to allow Modifying Applicable years val

--- resources/views/components/single-artifact-editor.blade.php
+++ resources/views/components/single-artifact-editor.blade.php
@@ -2,14 +2,17 @@
 
     @php
         //Get artifact value
+        $artifact_id = '0';
         $artifact_value = $artifact_value_default;
         $artifact_value_object = $artifactable->artifact($artifact_key);
         if ($artifact_value_object!=null && empty($artifact_value_object->value)==false){
+            $artifact_id = $artifact_value_object->id;
             $artifact_value = $artifact_value_object->value;
         }
     @endphp
 
     <a id="{{$control_id}}_btn_artifact" href="#" 
+        data-val-artifact-id="{{$artifact_id}}"
         data-val-artifactable-id="{{$artifactable->id}}"
         data-val-artifactable-name="{{$artifact_label}}"
         data-val-artifactable-class="{{get_class($artifactable)}}"
@@ -33,12 +36,13 @@
                     <form class="form-horizontal" id="{{$control_id}}_upload-form" method="post" enctype="multipart/form-data">
                         {!! csrf_field() !!}
 
+                        <input id="{{$control_id}}_artifacter_primary_id" type="hidden" value="0"/>
                         <input id="{{$control_id}}_artifacter_artifactable_id" type="hidden" value=""/>
                         <input id="{{$control_id}}_artifacter_artifactable_class" type="hidden" value=""/>
                         <input id="{{$control_id}}_artifacter_artifactable_name" type="hidden" value=""/>
 
                         <div class="mb-3">
-                            <input type="text" id="{{$control_id}}_value_text" class="form-control" />
+                            <input type="text" id="{{$control_id}}_value_text" class="form-control" value="{{ $artifact_value=='All' ? '' : $artifact_value }}" />
                         </div>
                     </form>
 
@@ -65,6 +69,7 @@
                 $('#{{$control_id}}_upload-form').trigger("reset");
                 $('#{{$control_id}}_artifacter-modal').modal('show');
 
+                $('#{{$control_id}}_artifacter_primary_id').val($(this).attr('data-val-artifact-id'));
                 $('#{{$control_id}}_artifacter_artifactable_id').val($(this).attr('data-val-artifactable-id'));
                 $('#{{$control_id}}_artifacter_artifactable_name').val($(this).attr('data-val-artifactable-name'));
                 $('#{{$control_id}}_artifacter_artifactable_class').val($(this).attr('data-val-artifactable-class'));
@@ -78,6 +83,9 @@
                 $('#{{$control_id}}_btn-add-artifacter').attr('disabled',true);
 
                 var formData = new FormData();
+                var actionType = "POST";
+                var endPointURL = "{{ route('fc-api.attributes.store') }}";
+
                 formData.append('_token', $('input[name="_token"]').val());
                 @if (isset($organization) && $organization!=null)
                     formData.append('organization_id', '{{$organization->id}}');
@@ -87,8 +95,17 @@
                 formData.append('key', "{{$artifact_key}}");
                 formData.append('value', $('#{{$control_id}}_value_text').val());
 
+                var artifacter_primary_id = $('#{{$control_id}}_artifacter_primary_id').val();
+                if (artifacter_primary_id != "0"){
+                    actionType = "PUT";
+                    formData.append('id', artifacter_primary_id);
+                    endPointURL = "{{ route('fc-api.attributes.update','') }}/"+artifacter_primary_id;
+                }
+                
+                formData.append('_method', actionType);
+
                 $.ajax({
-                    url: "{{ route('fc-api.attributes.store') }}",
+                    url: endPointURL,
                     type: "POST",
                     data: formData,
                     cache: false,
