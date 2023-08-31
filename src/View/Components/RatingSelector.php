5bb5c58a2d06779ed68e1837105222baea476662
Viktorzee <monsieurviktor@yahoo.com>
2023-08-31 11:46:33 +0100

    feature for only auth user to rate

--- src/View/Components/RatingSelector.php
+++ src/View/Components/RatingSelector.php
@@ -3,16 +3,20 @@
 namespace Hasob\FoundationCore\View\Components;
 
 use Illuminate\View\Component;
+use Illuminate\Support\Facades\Auth;
 
 class RatingSelector extends Component
 {
     public $control_id;
     public $ratable;
+    public $authCheck;
+    public $serviceCheck;
 
-    public function __construct($ratable)
+    public function __construct($ratable, $authCheck)
     {
         $this->ratable = $ratable;
         $this->control_id = "rtl_".time();
+        $this->authCheck = $authCheck;        
     }
 
     public function render()
